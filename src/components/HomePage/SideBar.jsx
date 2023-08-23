5490489a5482ad3a02f4920fd49fb03966fbbca2
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-08-23 10:40:30 -0500

    update render SideBar

--- src/components/HomePage/SideBar.jsx
+++ src/components/HomePage/SideBar.jsx
@@ -20,14 +20,14 @@ export class SideBar extends Component {
             <FontAwesomeIcon icon="fa-solid fa-list" />
           </button>
         </form>
-        <div className='navbar navbar-expand-md flex-row text-left'>
-          <nav className="collapse navbar-collapse sidebar-links py-1" id="SideBar">
+        <div className='navbar navbar-expand-md text-left'>
+          <nav className="collapse navbar-collapse flex-column sidebar-links" id="SideBar">
 
-            <div className="sidebar-item active mb-1">
-              <Link to={"/Start"} className="sidebar-link font-weight-bold px-2 py-1 d-block" >
+            <div className="sidebar-item active my-1 w-100">
+              <Link to={"/Start"} className="sidebar-link font-weight-bold px-0 py-1 px-md-2 btn btn-link" >
                 Get started
               </Link>
-              <ul className='nav flex-column sidebar-subnav'>
+              <ul className='navbar-nav flex-column sidebar-subnav'>
                 <li className="nav-item active">
                   <Link to={"/intro"} className="nav-link">Introduction</Link>
                 </li>
@@ -40,11 +40,11 @@ export class SideBar extends Component {
               </ul>
             </div>
 
-            <div className="sidebar-item mb-1">
-              <Link to={"/Layout"} className="sidebar-link font-weight-bold px-2 py-1 d-block" >
+            <div className="sidebar-item my-1 w-100">
+              <Link to={"/Layout"} className="sidebar-link font-weight-bold px-0 py-1 px-md-2 btn btn-link" >
                 Layout
               </Link>
-              <ul className='nav flex-column sidebar-subnav'>
+              <ul className='navbar-nav flex-column sidebar-subnav'>
                 <li className="nav-item active">
                   <Link to={"/overview"} className="nav-link">Overview</Link>
                 </li>
@@ -52,10 +52,28 @@ export class SideBar extends Component {
                   <Link to={"/grid"} className="nav-link">Grid</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to={"/utilitities"} className="nav-link">Utiliites</Link>
+                  <Link to={"/utilities"} className="nav-link">Utilities</Link>
                 </li>
               </ul>
             </div>
+
+            <div className="sidebar-item my-1 w-100">
+              <Link to={"/Components"} className="sidebar-link font-weight-bold px-0 py-1 px-md-2 btn btn-link" >
+                Components
+              </Link>
+              <ul className='navbar-nav flex-column sidebar-subnav'>
+                <li className="nav-item active">
+                  <Link to={"/alert"} className="nav-link">Alert</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/badge"} className="nav-link">Badge</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/dropdowns"} className="nav-link">Dropdowns</Link>
+                </li>
+              </ul>
+            </div>
+
           </nav>
         </div>
       </>
